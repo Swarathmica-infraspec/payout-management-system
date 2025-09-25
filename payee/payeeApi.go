0a738de7166f22ddc403003469866d2392e2164a
Swarathmica-infraspec <swarathmica@infraspec.dev>
2025-09-25 06:04:24 +0000

    Introduce get api

--- payee/payeeApi.go
+++ payee/payeeApi.go
@@ -2,93 +2,129 @@ package payee
 
 import (
 	"context"
-	"database/sql"
 	"encoding/json"
-	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"os"
-
-	_ "github.com/lib/pq"
+	"strconv"
+	"strings"
 )
 
-func PayeePostAPI(w http.ResponseWriter, r *http.Request) {
+type PayeeRequest struct {
+	Name     string `json:"name"`
+	Code     string `json:"code"`
+	AccNo    int    `json:"account_number"`
+	IFSC     string `json:"ifsc"`
+	Bank     string `json:"bank"`
+	Email    string `json:"email"`
+	Mobile   int    `json:"mobile"`
+	Category string `json:"category"`
+}
 
-	dsn := os.Getenv("TEST_DATABASE_URL")
+type PayeeGETResponse struct {
+	ID              int    `json:"id"`
+	BeneficiaryName string `json:"beneficiary_name"`
+	BeneficiaryCode string `json:"beneficiary_code"`
+	AccNo           int    `json:"account_number"`
+	IFSC            string `json:"ifsc_code"`
+	BankName        string `json:"bank_name"`
+	Email           string `json:"email"`
+	Mobile          int    `json:"mobile"`
+	PayeeCategory   string `json:"payee_category"`
+}
 
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err := db.Close(); err != nil {
-			log.Printf("failed to close db: %v", err)
-		}
-	}()
+func PayeePostAPI(store PayeeRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	store := PayeeDB(db)
-
-	type req struct {
-		Name     string `json:"name"`
-		Code     string `json:"code"`
-		AccNo    int    `json:"account_number"`
-		IFSC     string `json:"ifsc"`
-		Bank     string `json:"bank"`
-		Email    string `json:"email"`
-		Mobile   int    `json:"mobile"`
-		Category string `json:"category"`
-	}
-
-	var data req
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		http.Error(w, "Error unmarshaling JSON", http.StatusBadRequest)
-		return
-	}
-
-	p, err := NewPayee(data.Name, data.Code, data.AccNo, data.IFSC, data.Bank, data.Email, data.Mobile, data.Category)
-	if err != nil {
-		fmt.Println("Structure creation failed")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	_, err = store.Insert(context.Background(), p)
-	if err != nil {
-		fmt.Println(err)
-		fmt.Println("Insertion failed")
-		w.WriteHeader(http.StatusConflict)
-		if err := json.NewEncoder(w).Encode(map[string]string{
-			"error": "Payee cannot be created with duplicate values",
-		}); err != nil {
-			log.Printf("Failed to encode JSON response: %v", err)
+		var data PayeeRequest
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			http.Error(w, "Invalid JSON body", http.StatusBadRequest)
 			return
 		}
 
-		return
-	}
+		p, err := NewPayee(data.Name, data.Code, data.AccNo, data.IFSC, data.Bank, data.Email, data.Mobile, data.Category)
+		if err != nil {
+			http.Error(w, "Invalid payee data", http.StatusBadRequest)
+			return
+		}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(map[string]any{
-		"id": 1,
-	}); err != nil {
-		http.Error(w, "failed to encode response", http.StatusInternalServerError)
-		return
-	}
+		id, err := store.Insert(context.Background(), p)
+		if err != nil {
+			http.Error(w, "DB insertion failed: "+err.Error(), http.StatusConflict)
+			return
+		}
 
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		_ = json.NewEncoder(w).Encode(map[string]any{"id": id})
+	}
 }
 
-func SetupRouter() *http.ServeMux {
+func PayeeGetAPI(store PayeeRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		payees, err := store.List(context.Background())
+		if err != nil {
+			http.Error(w, "DB query failed: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var resp []PayeeGETResponse
+		for _, p := range payees {
+			resp = append(resp, PayeeGETResponse{
+				ID:              p.id,
+				BeneficiaryName: p.beneficiaryName,
+				BeneficiaryCode: p.beneficiaryCode,
+				AccNo:           p.accNo,
+				IFSC:            p.ifsc,
+				BankName:        p.bankName,
+				Email:           p.email,
+				Mobile:          p.mobile,
+				PayeeCategory:   p.payeeCategory,
+			})
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(resp)
+	}
+}
+
+func PayeeGetOneAPI(store PayeeRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		idStr := strings.TrimPrefix(r.URL.Path, "/payees/")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "invalid id", http.StatusBadRequest)
+			return
+		}
+
+		p, err := store.GetByID(context.Background(), id)
+		if err != nil {
+			http.Error(w, "record not found", http.StatusNotFound)
+			return
+		}
+
+		resp := PayeeGETResponse{
+			ID:              p.id,
+			BeneficiaryName: p.beneficiaryName,
+			BeneficiaryCode: p.beneficiaryCode,
+			AccNo:           p.accNo,
+			IFSC:            p.ifsc,
+			BankName:        p.bankName,
+			Email:           p.email,
+			Mobile:          p.mobile,
+			PayeeCategory:   p.payeeCategory,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(resp)
+	}
+}
+func SetupRouter(store PayeeRepository) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/payees", PayeePostAPI)
+	mux.HandleFunc("/payees", PayeePostAPI(store))
+	mux.HandleFunc("/payees/list", PayeeGetAPI(store))
+	mux.HandleFunc("/payees/", PayeeGetOneAPI(store))
 	return mux
 }
--- payee/payeeApi_test.go
+++ payee/payeeApi_test.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 
 	_ "github.com/lib/pq"
@@ -19,7 +21,9 @@ func initStore() PayeeRepository {
 		return store
 	}
 	dsn := os.Getenv("TEST_DATABASE_URL")
-
+	if dsn == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		panic(err)
@@ -45,7 +49,7 @@ func setupMux(t *testing.T) *http.ServeMux {
 		t.Fatalf("failed to clean DB: %v", err)
 	}
 
-	mux := SetupRouter()
+	mux := SetupRouter(store)
 
 	return mux
 }
@@ -106,7 +110,7 @@ func TestPayeePostAPIInvalidJSON(t *testing.T) {
 	}
 
 	resp := w.Body.String()
-	expected := "Error unmarshaling JSON\n"
+	expected := "Invalid JSON body\n"
 
 	if resp != expected {
 		t.Fatalf("expected body %q, got %q", expected, resp)
@@ -143,8 +147,170 @@ func TestPayeePostAPIDuplicate(t *testing.T) {
 		t.Fatalf("expected 409 Conflict, got %d, body=%s", w2.Code, w2.Body.String())
 	}
 
-	expected := `{"error":"Payee cannot be created with duplicate values"}` + "\n"
+	expected := "DB insertion failed: pq: duplicate key value violates unique constraint \"payees_beneficiary_code_key\"\n"
 	if w2.Body.String() != expected {
 		t.Fatalf("expected body %q, got %q", expected, w2.Body.String())
+	}
+}
+
+func TestPayeeGetAPI(t *testing.T) {
+	mux := setupMux(t)
+
+	payload := map[string]interface{}{
+		"name":           "Abdc",
+		"code":           "1262",
+		"account_number": 1234767893,
+		"ifsc":           "CBIN0123456",
+		"bank":           "CBI",
+		"email":          "abcd@example.com",
+		"mobile":         9876543292,
+		"category":       "Employee",
+	}
+	body, _ := json.Marshal(payload)
+	reqCreate := httptest.NewRequest(http.MethodPost, "/payees", bytes.NewBuffer(body))
+	wCreate := httptest.NewRecorder()
+	mux.ServeHTTP(wCreate, reqCreate)
+	if wCreate.Code != http.StatusCreated {
+		t.Fatalf("expected 201 Created, got %d, body=%s", wCreate.Code, wCreate.Body.String())
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/payees/list", nil)
+	w := httptest.NewRecorder()
+	mux.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Fatalf("expected status %d, got %d, body=%s", http.StatusOK, w.Code, w.Body.String())
+	}
+
+	var resp []PayeeGETResponse
+	if err := json.Unmarshal(w.Body.Bytes(), &resp); err != nil {
+		t.Fatalf("response is not valid JSON: %v", err)
+	}
+
+	if len(resp) != 1 {
+		t.Fatalf("expected 1 payee, got %d", len(resp))
+	}
+
+	got := resp[0]
+	if got.BeneficiaryName != payload["name"] {
+		t.Errorf("expected name %q, got %q", payload["name"], got.BeneficiaryName)
+	}
+	if got.BeneficiaryCode != payload["code"] {
+		t.Errorf("expected code %q, got %q", payload["code"], got.BeneficiaryCode)
+	}
+	if got.AccNo != payload["account_number"] {
+		t.Errorf("expected account_number %v, got %v", payload["account_number"], got.AccNo)
+	}
+	if got.IFSC != payload["ifsc"] {
+		t.Errorf("expected IFSC %q, got %q", payload["ifsc"], got.IFSC)
+	}
+	if got.BankName != payload["bank"] {
+		t.Errorf("expected bank %q, got %q", payload["bank"], got.BankName)
+	}
+	if got.Email != payload["email"] {
+		t.Errorf("expected email %q, got %q", payload["email"], got.Email)
+	}
+	if got.Mobile != payload["mobile"] {
+		t.Errorf("expected mobile %v, got %v", payload["mobile"], got.Mobile)
+	}
+	if got.PayeeCategory != payload["category"] {
+		t.Errorf("expected category %q, got %q", payload["category"], got.PayeeCategory)
+	}
+
+}
+
+func TestPayeeGetOneAPI(t *testing.T) {
+	mux := setupMux(t)
+
+	payload := map[string]interface{}{
+		"name":           "Abdc",
+		"code":           "1262",
+		"account_number": 1234767893,
+		"ifsc":           "CBIN0123456",
+		"bank":           "CBI",
+		"email":          "abcd@example.com",
+		"mobile":         9876543292,
+		"category":       "Employee",
+	}
+	body, _ := json.Marshal(payload)
+
+	reqCreate := httptest.NewRequest(http.MethodPost, "/payees", bytes.NewBuffer(body))
+	wCreate := httptest.NewRecorder()
+	mux.ServeHTTP(wCreate, reqCreate)
+
+	if wCreate.Code != http.StatusCreated {
+		t.Fatalf("expected 201 Created, got %d, body=%s", wCreate.Code, wCreate.Body.String())
+	}
+
+	type CreateResp struct {
+		ID int `json:"id"`
+	}
+	var createResp CreateResp
+	if err := json.Unmarshal(wCreate.Body.Bytes(), &createResp); err != nil {
+		t.Fatal("Failed to unmarshal create response:", err)
+	}
+
+	url := "/payees/" + strconv.Itoa(createResp.ID)
+	reqGet := httptest.NewRequest(http.MethodGet, url, nil)
+	wGet := httptest.NewRecorder()
+	mux.ServeHTTP(wGet, reqGet)
+
+	if wGet.Code != http.StatusOK {
+		t.Fatalf("expected 200 OK, got %d, body=%s", wGet.Code, wGet.Body.String())
+	}
+
+	var getResp PayeeGETResponse
+
+	if err := json.Unmarshal(wGet.Body.Bytes(), &getResp); err != nil {
+		t.Fatal("Failed to unmarshal get response:", err)
+	}
+
+	if getResp.ID != createResp.ID {
+		t.Fatalf("expected ID %d, got %d", createResp.ID, getResp.ID)
+	}
+	if getResp.BeneficiaryName != payload["name"] {
+		t.Fatalf("expected name %q, got %q", payload["name"], getResp.BeneficiaryName)
+	}
+	if getResp.BeneficiaryCode != payload["code"] {
+		t.Fatalf("expected code %q, got %q", payload["code"], getResp.BeneficiaryCode)
+	}
+	if getResp.AccNo != payload["account_number"] {
+		t.Fatalf("expected accNo %v, got %v", payload["account_number"], getResp.AccNo)
+	}
+	if getResp.IFSC != payload["ifsc"] {
+		t.Fatalf("expected IFSC %q, got %q", payload["ifsc"], getResp.IFSC)
+	}
+	if getResp.BankName != payload["bank"] {
+		t.Fatalf("expected bank %q, got %q", payload["bank"], getResp.BankName)
+	}
+	if getResp.Email != payload["email"] {
+		t.Fatalf("expected email %q, got %q", payload["email"], getResp.Email)
+	}
+	if getResp.Mobile != payload["mobile"] {
+		t.Fatalf("expected mobile %v, got %v", payload["mobile"], getResp.Mobile)
+	}
+	if getResp.PayeeCategory != payload["category"] {
+		t.Fatalf("expected category %q, got %q", payload["category"], getResp.PayeeCategory)
+	}
+}
+
+func TestPayeeGetOneAPINotFound(t *testing.T) {
+	mux := setupMux(t)
+
+	nonExistentID := 9999
+	url := "/payees/" + strconv.Itoa(nonExistentID)
+
+	req := httptest.NewRequest(http.MethodGet, url, nil)
+	w := httptest.NewRecorder()
+
+	mux.ServeHTTP(w, req)
+
+	if w.Code != http.StatusNotFound {
+		t.Fatalf("expected 404 Not Found, got %d, body=%s", w.Code, w.Body.String())
+	}
+
+	expected := "record not found\n"
+	if w.Body.String() != expected {
+		t.Fatalf("expected body %q, got %q", expected, w.Body.String())
 	}
 }
