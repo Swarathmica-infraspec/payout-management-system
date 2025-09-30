8cb8dd1d37f27fe1e93ea2cdf612dbfb52d263cc
Swarathmica-infraspec <swarathmica@infraspec.dev>
2025-09-30 05:52:36 +0000

    chore: apply formatting

--- payee/payeeDAO_test.go
+++ payee/payeeDAO_test.go
@@ -72,7 +72,7 @@ func TestGetPayeeByID(t *testing.T) {
 		INSERT INTO payees (beneficiary_name, beneficiary_code, account_number, ifsc_code, bank_name, email, mobile, payee_category)
 		VALUES ('Abc','136',1234567890123456,'CBIN0123459','CBI','abc@gmail.com',9123456780,'Employee')
 		RETURNING id`).Scan(&id)
-		
+
 	require.NoError(t, err, "failed to insert payee")
 	defer func() {
 		_, err := db.Exec("DELETE FROM payees WHERE id = $1", id)
